d612c944791035cca42edd0ced1b2cf61f0960aa
soho74 <nori.h.nori@gmail.com>
2018-05-24 20:23:33 +0900

    update classifier

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,10 +1,57 @@
 import cv2
+import numpy as np
 import tensorflow as tf
 from styx_msgs.msg import TrafficLight
-
+import rospy
 
 class TLClassifier(object):
     def __init__(self):
+        print(tf.__version__)
+
+        self.detection_graph = tf.Graph()
+
+        with self.detection_graph.as_default() as graph:
+          od_graph_def = tf.GraphDef()
+          with tf.gfile.GFile("light_classification/tlc_model/frozen_inference_graph.pb", 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+
+          with tf.Session() as sess:
+            self.tensor_dict = {}
+            self.tensor_dict['detection_scores'] = tf.get_default_graph().get_tensor_by_name('detection_scores:0')
+            self.tensor_dict['detection_classes'] = tf.get_default_graph().get_tensor_by_name('detection_classes:0')    
+            self.image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+
+        self.sess = tf.Session(graph=graph)                          
+
+            
+    def get_classification(self, image):
+
+        image = cv2.resize(image[:,:,::-1],(200, 200), cv2.INTER_CUBIC)
+        final_image = np.expand_dims(image, 0)
+
+        output_dict = self.sess.run(self.tensor_dict, feed_dict={self.image_tensor: final_image})
+
+        output_dict['detection_classes'] = output_dict['detection_classes'][0].astype(np.uint8)
+        output_dict['detection_scores'] = output_dict['detection_scores'][0]
+
+        score = output_dict['detection_scores'][0]
+
+        if score > 0.4:
+            num = output_dict['detection_classes'][0] -1
+        else:
+            num = 6
+
+        print("                                                                                  ", num, score)
+
+        if num == 6:
+            return 4
+        else:
+            return num % 3
+        return num
+
+    def __init__2(self):
         #TODO load classifier        
         with tf.gfile.GFile('light_classification/tlc_model/tlc_model.pb', "rb") as f:
             graph_def = tf.GraphDef()
@@ -23,7 +70,7 @@ class TLClassifier(object):
             self.sess = tf.Session(graph=graph)
             self.counter = 0
             
-    def get_classification(self, image):
+    def get_classification2(self, image):
         """Determines the color of the traffic light in the image
         Args:
             image (cv::Mat): image containing the traffic light
@@ -44,4 +91,3 @@ class TLClassifier(object):
         print self.counter, light[lid]
         
         return lid
-
